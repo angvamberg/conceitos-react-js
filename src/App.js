db2f63e177f0f427ef6306781f05cb2a6a0f65e9
Angleby Vieira <angleby@gmail.com>
2020-08-16 03:01:28 -0300

    Implementando adicionar e remover via axios

--- src/App.js
+++ src/App.js
@@ -1,31 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    getAllRepositories(setRepositories);
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const newRepository = {
+      title: `New Repository - ${Date.now()}`,
+      url: `${Date.now()}`,
+      techs: [`${Date.now()}`, `${Date.now()}`]
+    };
+
+    const response = await api.post('repositories', newRepository);
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    setRepositories(repositories.filter(repository => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+      {repositories.map(repository => 
+        <li key={repository.id}>
+          {repository.title}
+          <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>
+      )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
+}
+
+function getAllRepositories(setRepositories) {
+  api.get('repositories').then(response => setRepositories(response.data));
 }
 
 export default App;
